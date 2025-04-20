1748bb47030affd98d4d48a8733e48e515dc021a
007hyno <Parasrawat05@gmail.com>
2025-04-20 12:19:58 +0530

    Added new profile picture and grammer check

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     ['https://findvalentineon.web.app/', './utils/valantineFinder.png', 'Valantine Finder', 'Meet people with common interests.','./utils/web.png','Web'],
     ['https://play.google.com/store/apps/details?id=com.paras.aartisangrah', './utils/AartiSangrah.png', 'Aarti Sangrah', 'Understand aarti and Hanuman Chalisa with Hindi and English meanings.','./utils/android1.png','Android'],
     ['https://github.com/007hyno/Social-Media', './utils/SocialMedia.png', 'Social Media', 'SPA(Single Page Application) for communities. ','./utils/web.png','Web'],
-    ['https://007hyno.github.io/find-vac.github.io/', './utils/vaccination.png', 'Vaccination (Depriciated)', 'Find vacant Covid Vaccination slots near you','./utils/web.png','Web'],
+    ['https://007hyno.github.io/find-vac.github.io/', './utils/vaccination.png', 'Vaccination (Deprecated)', 'Find vacant Covid Vaccination slots near you','./utils/web.png','Web'],
     ['https://play.google.com/store/apps/details?id=com.paras.ssbprep', './utils/ssb.png', 'SSB Quest: SSB Prepration', 'Free SSB Prepration App 📚Mock Tests, ⌛Stopwatch, 📖Study Material: Articles & Videos.','./utils/android1.png','Android'],
   ];
 
